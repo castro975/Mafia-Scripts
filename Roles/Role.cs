706037ed291c3b189174a9587d2cff7e4742e937
Z13J <Z13J@ymail.com>
2013-07-22 21:11:12 -0700

    Comments on bools
    Added comments describing attributes in the bools

--- Roles/Role.cs
+++ Roles/Role.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 using System.Collections;
 
-public struct Role{
+public class Role{
+	#region Information
 	public string roleName;
+	
 	public enum Alliance{
 		Town,
 		Mafia,
 		None
 	}
 	public Alliance alliedTeam;
+	#endregion
 	
-	//Testing GitHub commits and such
+	#region Attributes
+	public bool canAttackAtNight;		//Can our character attack a character at night or not (True or false)
+	public bool canHeal;				//Can our character heal a character at night or not (True or false)
+	public bool isInvulnerableAtNight;	//Is our character able to be attacked at night or not (True or false)
+	#endregion
 }
-
